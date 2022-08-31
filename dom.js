5b2d80b059a56ab718e92c1e4bb678c38f62aaeb
Swarnalata-Rana <swarnalatarana2@gmail.com>
2022-08-31 14:25:37 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,88 @@
+// DOM GET METHODS
+innerText
+// var element;
+// element=document.getElementById("content").innerText;
+// console.log(element)
+
+
+innerHTML
+// var element;
+// element=document.getElementById("content").innerHTML;
+// console.log(element)
+
+getAttribute
+// var element;
+// element=document.getElementById("header").getAttribute("class");
+// console.log(element)
+
+getAttributeNode
+// var element;
+// element=document.getElementById("header").getAttributeNode("onClick");
+// console.log(element)
+
+
+getAttributes
+// var element;
+// element=document.getElementById("header").attributes[2].value; 
+// console.log(element)
+// if u want that atrribut name u use .name it come
+
+
+// DOM SET METHODS
+innerText
+// var element;
+// element=document.getElementById("header").innerText="Runu"
+// console.log(element)
+
+innerHTML
+// var element;
+// element=document.getElementById("header").innerHTML="<h1>runu</h1>"
+// console.log(element)
+
+// setAttribut ---2 attributes asiba name & value
+// var element;
+// document.getElementById("header").setAttribute("style","border:10px dotted yellow;")
+// console.log(element)
+
+attribute
+// var element;
+// document.getElementById("header").attributes[1].value="Runu"
+// console.log(element)
+
+removeattribute
+// var element;
+// element=document.getElementById("header").removeAttribute("style")
+// console.log(element)
+// var element;
+// element=document.getElementById("id");
+// console.log(element)
+
+
+// CSS 
+// // style
+// var element;
+// element=document.querySelector("#header").style.backgroundColor="blue";
+// console.log(element)
+
+
+// className
+// var element;
+// element=document.querySelector(".list");
+// console.log(element)
+
+// classList
+// var element;
+// element=document.querySelectorAll(".list");
+// console.log(element)
+
+
+//  copy
+// var element;
+// element=document.createElement("p");
+// element.innerText="This is Runu";
+// Document.body.appendChild(element)
+//  console.log(element)
+
+
+
+
